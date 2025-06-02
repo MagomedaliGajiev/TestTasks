09c958ad7f9b63d53e9f9ef4c20917001eb36d92
Magomedali Gajiev <mag198421@gmail.com>
2025-06-02 17:22:16 +0300

    Добавил задачу 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1
+//Дана строка, содержащая n маленьких букв латинского алфавита. Требуется реализовать алгоритм компрессии этой строки, замещающий группы последовательно идущих одинаковых букв формой "sc" (где "s" – символ, "с" – количество букв в группе), а также алгоритм декомпрессии, возвращающий исходную строку по сжатой.
+
+//Если буква в группе всего одна – количество в сжатой строке не указываем, а пишем её как есть.
+
+//Пример:
+
+//Исходная строка: aaabbcccdde
+//Сжатая строка: a3b2c3d2e
+public class Program
+{
+    public static void Main()
+    {
+        var original = Console.ReadLine();
+        var compressed = StringCompressor.Compress(original);
+        var decompressed = StringCompressor.Decompress(compressed);
+
+        Console.WriteLine("Original: " + original);
+        Console.WriteLine("Compressed: " + compressed);
+        Console.WriteLine("Decompressed: " + decompressed);
+    }
+}
---
+++ Task1/StringCompressor.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+public class StringCompressor
+{
+    public static string Compress(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return string.Empty;
+
+        var compressed = new StringBuilder();
+        var count = 1;
+
+        for (int i = 1; i <= input.Length; i++)
+        {
+            if (i < input.Length && input[i] == input[i - 1])
+            {
+                count++;
+            }
+            else
+            {
+                compressed.Append(input[i - 1]);
+                if (count > 1)
+                    compressed.Append(count);
+                count = 1;
+            }
+        }
+
+        return compressed.ToString();
+    }
+
+    public static string Decompress(string compressed)
+    {
+        if (string.IsNullOrEmpty(compressed))
+            return string.Empty;
+
+        var decompressed = new StringBuilder();
+        var index = 0;
+
+        while (index < compressed.Length)
+        {
+            var currentChar = compressed[index++];
+            if (index < compressed.Length && char.IsDigit(compressed[index]))
+            {
+                var numStart = index;
+                while (index < compressed.Length && char.IsDigit(compressed[index]))
+                    index++;
+
+                var numStr = compressed.Substring(numStart, index - numStart);
+                var count = int.Parse(numStr);
+                decompressed.Append(currentChar, count);
+            }
+            else
+            {
+                decompressed.Append(currentChar);
+            }
+        }
+
+        return decompressed.ToString();
+    }
+}
